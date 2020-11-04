44d2635e5f243eb50444206268b7a27da0a0fefa
Johan Félisaz <johan.felisaz@protonmail.com>
2020-11-04 11:55:06 +0100

    first rule for zoom

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-/*
-  Called when the item has been created, or when creation failed due to an error.
-  We'll just log success/failure here.
-*/
+// -----------------------------------------------------------------------------
+// General hook for the context menu button
+// -----------------------------------------------------------------------------
+
+// Called when the item has been created, or when creation failed due to an error.
+// We'll just log success/failure here.
 function onCreated() {
     if (browser.runtime.lastError) {
 	console.log(`Error: ${browser.runtime.lastError}`);
@@ -10,23 +12,45 @@ function onCreated() {
     }
 }
 
-/*
-  Create all the context menu items.
-*/
 browser.menus.create({
     id: "log-selection",
-    title: browser.i18n.getMessage("menuItemSelectionLogger"),
+    title: browser.i18n.getMessage("menuItemParseEvent"),
     contexts: ["selection"]
 }, onCreated);
 
-/*
-  The click event listener, where we perform the appropriate action given the
-  ID of the menu item that was clicked.
-*/
 browser.menus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
     case "log-selection":
+	// For now just print the output of the parsing step
 	console.log(info.selectionText);
+	console.log(parseEvent(info.selectionText));
 	break;
     }
 });
+
+// -----------------------------------------------------------------------------
+// The parsing function itself
+// -----------------------------------------------------------------------------
+
+// Returns an object with several fields that it managed to parse
+function parseEvent(str)
+{
+    output = {str: str};
+    
+    zoomLink = parseZoom(str);
+    if(zoomLink)
+	output.zoomLink = zoomLink;
+    
+    return output;
+}
+
+function parseZoom(str)
+{
+    var re = /epfl\.zoom\.us\/j\/(\d+)/;
+    res = str.match(re);
+
+    if(res)
+	return "https://epfl.zoom.us/j/" + res[1];
+
+    return null;
+}
